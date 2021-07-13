23e7c6628483561e679cf2eed9dc62abc0d8d1b4
borislav nikolov <borislav@fastmail.com>
2021-07-13 17:36:33 +0200

    sort properly

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 echo > README.md
-for i in  cover.md hello.md $(ls -1 week-*.md | sort -n); do
+for i in  cover.md hello.md $(ls -1 week-*.md | sort -k 2 -t- -n); do
     cat $i >> README.md
     echo >> README.md
 done
