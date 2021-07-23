9793706deb63d8a022e7cec079145e8d45b0e87d
borislav nikolov <borislav@fastmail.com>
2021-07-23 23:03:35 +0200

    Revert "Merge pull request #2 from colevandersWands/book-build" [ its too much to have each commit generate the book, its 5mb ]
    This reverts commit 767ea256e18f911aaf3c7ff5007a114243a6c517, reversing
    changes made to 4566a1ba5bc59b8b6ca0c217c12704680fe22605.

--- book-build.js
+++
@@ -1,8 +0,0 @@
-const prettyMdPdf = require("pretty-markdown-pdf");
-
-prettyMdPdf.convertMd({
-  markdownFilePath: "./README.md",
-  outputFilePath: "./book.pdf",
-  stylesRelativePathFile: true,
-  styles: ["./style.css"],
-});
