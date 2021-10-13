23a674690bbd97ec05d951984ab55709a08e2b82
borislav nikolov <borislav@fastmail.com>
2021-10-13 21:55:55 +0200

    toc

--- toc.py
+++ toc.py
@@ -24,5 +24,5 @@ for fn in dir:
                 clear = line.lower().replace('## ','').replace('[','').replace(']','')
                 toc.write('\n['+clear+'](#'+clear.replace(' ','-')+')\n')
             if line.startswith('!['):
-                toc.write(line.replace(')','=250x)') + '\n')
+                toc.write(line.replace(')',' =250x)') + '\n')
 toc.close()
