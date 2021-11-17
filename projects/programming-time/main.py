4d1b16c47b643fb2e5d353faa390c6d728a95c18
borislav nikolov <borislav@fastmail.com>
2021-11-17 16:07:23 +0100

    wip

--- projects/programming-time/main.py
+++ projects/programming-time/main.py
@@ -5,12 +5,12 @@ import sys
 from itertools import cycle
 from PIL import Image, ImageDraw, ImageFont
 import time
-import easy,medium,hard
+import intro,easy,medium,hard
 
-WIDTH = 1058
-HEIGHT = 671
-COLS = 33
-ROWS = 28
+HEIGHT = 1039
+WIDTH = 744
+COLS = 37
+ROWS = 27
 fnt = ImageFont.truetype('font.ttf', 35)
 
 fgcolor = (20, 20, 20, 255)
@@ -21,15 +21,13 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
-def border(d, difficulty, data, id):
+def border(d, data, id):
     lines = []
-    bottom = '+-------------------------------+'
-    top = '+----------->' + str(id).zfill(2) + ' @ ' + \
-        str(difficulty).zfill(2)+'<-----------+'
-    if difficulty == 0:
+    bottom = '+-----------------------------------+'
+    top = '+--------------->' + str(id).zfill(3) + '<---------------+'
+    if id == 0:
         top = bottom
     lines.append(top)
-
 
     text = data.split('\n')
     for i in range(ROWS):
@@ -43,42 +41,44 @@ def border(d, difficulty, data, id):
     help = (20, 20, 20, 20)
     size = 2
     d.rectangle([0, 0, size, size], fill=help)
-    d.rectangle([HEIGHT-size, WIDTH-size, HEIGHT, WIDTH], fill=help)
-    d.rectangle([0, WIDTH-size, size, WIDTH], fill=help)
-    d.rectangle([HEIGHT-size, 0, HEIGHT, size], fill=help)
-    d.multiline_text((45, 18),"\n".join(lines), font=fnt, fill=fgcolor)
+    d.rectangle([WIDTH-size, HEIGHT-size, WIDTH, HEIGHT], fill=help)
+    d.rectangle([0, HEIGHT-size, size, HEIGHT], fill=help)
+    d.rectangle([WIDTH-size, 0, WIDTH, size], fill=help)
+    d.multiline_text((47, 24),"\n".join(lines), font=fnt, fill=fgcolor)
 
 
-def back(id, difficulty, numbers):
-    img = Image.new('CMYK', (HEIGHT, WIDTH), color=bgcolor)
+def back(deck, id, numbers):
+    img = Image.new('CMYK', (WIDTH, HEIGHT), color=bgcolor)
     d = ImageDraw.Draw(img)
     lines = []
     for n in numbers:
         lines.append(str(n).rjust(random.randint(0,COLS - 3), ' '))
 
-    border(d, 0, "\n".join(lines), 0)
+    border(d, "\n".join(lines), 0)
       
-    img.save(os.path.join('images', 'back_card_' +
+    img.save(os.path.join('images', deck, 'back_card_' +
              str(id).zfill(3)+'.tiff'), compression="tiff_lzw")
 
 
-def front(id, difficulty, code):
-    img = Image.new('CMYK', (HEIGHT, WIDTH), color=bgcolor)
+def front(deck, id, code):
+    img = Image.new('CMYK', (WIDTH, HEIGHT), color=bgcolor)
     d = ImageDraw.Draw(img)
-    border(d, difficulty, code, id)
-    img.save(os.path.join('images', 'front_card_' +
+    border(d, code, id)
+    img.save(os.path.join('images', deck,'front_card_' +
              str(id).zfill(3)+'.tiff'), compression="tiff_lzw")
 
 
-def cheat(answers, numbers):
+def cheat(deck, answers, numbers):
+    i = 0
     for (n, a) in enumerate(list(chunks(answers, ROWS))):
-        img = Image.new('CMYK', (HEIGHT, WIDTH), color=bgcolor)
+        img = Image.new('CMYK', (WIDTH, HEIGHT), color=bgcolor)
         d = ImageDraw.Draw(img)
-        border(d, 0, "\n".join(a), 0)
+        border(d, "\n".join(a), 0)
         img.save(os.path.join(
-            'images', 'front_card_answers_'+str(n).zfill(3)+'.tiff'), compression="tiff_lzw")
-
-        back(800 + n, 0, numbers)
+            'images', deck, 'front_card_answers_'+str(n).zfill(3)+'.tiff'), compression="tiff_lzw")
+        i += 1
+        back(deck, 0, numbers)
+    return i
 
 
 def run(code):
@@ -95,41 +95,44 @@ def run(code):
 
 
 
-cards = [*easy.cards,*medium.cards, *hard.cards]
 
 
-print('easy',len(easy.cards))
-print('medium',len(medium.cards))
-print('hard',len(hard.cards))
-print('total',len(cards))
+for (deck, cards) in [['easy', [*intro.cards, *easy.cards]],['medium',[*intro.cards,*medium.cards]], ['hard',[*intro.cards,*hard.cards]]]:
+    path = os.path.join('images', deck)
+    try:
+        os.mkdir(path)
+    except:
+        pass
+    print('printing', deck, 'deck, with',len(cards),'cards')
+    seen = {}
+    qa = []
+    possible = set()
+    for (_i, card) in enumerate(cards):
+        if card in seen:
+            raise Exception("ALREADY SEEN: " + card)
+        seen[card] = True
+        _out = run(card).strip().split("\n")
+        if len(_out) == 0:
+            raise "NO OUTPUT: " + card
+        for line in _out:
+            possible.add(line)
+            qa.append(str(_i).zfill(3) + ": " + line)
 
-cards.sort(key=lambda x:x[0])
-seen = {}
-qa = []
-possible = set()
-for (_i, card) in enumerate(cards):
-    if card[1] in seen:
-      raise Exception("ALREADY SEEN: " + str(card[0]) + " -> " + card[1])
-    seen[card[1]] = True
-    _out = run(card[1]).strip().split("\n")
-    if len(_out) == 0:
-        raise "NO OUTPUT: " + card[1]
-    for line in _out:
-        possible.add(line)
-        qa.append(str(_i).zfill(3) + ": " + line)
+    random.seed(time.time())
+    shuffled = list(possible)
+    print('possible results', len(possible), 'rows', ROWS)
 
+    for (i, card) in enumerate(cards):
+        random.shuffle(shuffled)
+        rotate = cycle(shuffled)
+        numbers = []
+        for x in range(ROWS):
+            numbers.append(next(rotate))
+        front(deck, i, card)
+        back(deck, i, numbers)
 
-random.seed(time.time())
-shuffled = list(possible)
-print('possible results', len(possible), 'rows', ROWS)
+    cheatsheet = cheat(deck, qa, numbers)
 
-for (i, card) in enumerate(cards):
-    random.shuffle(shuffled)
-    rotate = cycle(shuffled)
-    numbers = []
-    for x in range(ROWS):
-        numbers.append(next(rotate))
-    front(i, card[0], card[1])
-    back(i, card[0], numbers)
-
-cheat(qa, numbers)
+    a1 = 55
+    print(deck, 'total number of cards:', cheatsheet + len(cards), 'cheatsheet:', cheatsheet, 'missing:', a1 -  (cheatsheet + len(cards)))
+    print('*' * 40)
