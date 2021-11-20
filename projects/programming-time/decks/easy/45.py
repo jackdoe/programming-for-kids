d376ea7cda9b142227e2814ad2d6e350a8c00c36
borislav nikolov <borislav@fastmail.com>
2021-11-20 18:07:12 +0100

    45

---
+++ projects/programming-time/decks/easy/45.py
@@ -0,0 +1,22 @@
+# | computer memory |
+# |.................|
+# |....0............|
+# |....^............|
+# +----+------------+
+#      |
+#      |
+# a ---+
+# 
+# a = a + 1 means the computer
+# has to read the current value
+# of 'a', add 1 to it, and store
+# it back to 'a'
+
+a = 0
+
+a = a + 1
+
+# same as a = a + 1
+a += 1
+
+print(a)
