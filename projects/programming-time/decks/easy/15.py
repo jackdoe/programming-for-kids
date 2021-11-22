d14a3de668f2b3365c435bd1e76044821059d83b
borislav nikolov <borislav@fastmail.com>
2021-11-23 00:02:37 +0100

    wip

--- projects/programming-time/decks/easy/15.py
+++ projects/programming-time/decks/easy/15.py
@@ -1,4 +1,22 @@
-a = 'hello world'
-b = a[2:]
+# | computer memory  |
+# |..................|
+# |....2 1 55..1 55..|
+# |....^.......^.....|
+# |....|.......|.....|
+# +----+-------+-----+
+#      |       |
+# a ---+       |
+# b -----------+
+#
+# 55 is the ASCII for the number 7
+# Even though the raw values are
+# the same, the types are not, so
+# python knows that '7' is not 55.
 
-print(len(b))
+a = '7'
+b = 55
+
+if a == b:
+  print('seven')
+else:
+  print(7)
