2435e1607752bddc883b58ae76403e9741cbad60
borislav nikolov <borislav@fastmail.com>
2021-11-22 23:48:19 +0100

    wip

--- projects/programming-time/decks/easy/13.py
+++ projects/programming-time/decks/easy/13.py
@@ -1,6 +1,23 @@
-def hello():
-  return 'world'
+# | computer memory        |
+# |........................|
+# |....2 1 55..............|
+# |....^.......2 2 55 55...|
+# |....|.......^...........|
+# +----+-------+-----------+
+#      |       |
+# a ---+       |
+# b -----------+
+#
+# 55 is the ASCII for the number 7
+# To compare two strings, first
+# you compare their length, and
+# if its the same, you compare
+# character by character.
 
-a = hello()
+a = '7'
+b = '77'
 
-print(len(a))
+if a == b:
+  print(a)
+else:
+  print(b)
