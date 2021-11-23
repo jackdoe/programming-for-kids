8a7ccf3ac1a5187c5c93014495f6e2dd195cc6cc
borislav nikolov <borislav@fastmail.com>
2021-11-23 13:01:52 +0100

    wip

--- projects/programming-time/decks/easy/00.py
+++ projects/programming-time/decks/easy/00.py
@@ -1,7 +1,7 @@
 # PROGRAMMING TIME
 #
 # Start with 1 card per player,
-# put the rest face down (numbers 
+# put the rest face down (numbers
 # up).
 #
 # Each player has to interpred the
@@ -13,7 +13,15 @@
 # output, takes  the card from
 # the deck and it becomes their
 # active card.
-# 
+#
 # Player with most cards wins.
+
+# Cheat Sheet:
+#   Value Type      |    ASCII
+#   1 -> integer    |    a -> 97
+#   2 -> string     |    z -> 122
+#   3 -> range      |    0 -> 48
+#   4 -> list       |    9 -> 57
+#   5 -> boolean    |
 
 print(3)
--- projects/programming-time/decks/easy/16.py
+++ projects/programming-time/decks/easy/16.py
@@ -1,7 +1,21 @@
-# 
-# len('abc') -> 3
+# | computer memory  |
+# |..................|
+# |....2 1 98........|
+# |....^.............|
+# |....|.............|
+# +----+-------------+
+#      |
+# a ---+
 #
+# 98 is the ASCII for the letter
+# 'b', len(string) returns the
+# length of the string. So the
+# computer will go to the memory
+# where the variable 'a' points,
+# and skip the type(2 for string)
+# slot and return the length
+# stored next to it(1).
 
-b = 8
+a = 'b'
 
-print(len('hi' * b))
+print(len(a))
