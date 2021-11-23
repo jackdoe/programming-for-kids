d811545fe3f38c5a7605f385160cc58ddf71b5a5
borislav nikolov <borislav@fastmail.com>
2021-11-23 20:56:40 +0100

    explain

--- projects/programming-time/decks/easy/24.py
+++ projects/programming-time/decks/easy/24.py
@@ -1,7 +1,20 @@
+# A dictionary is a kind of a list
+# where you can access things
+# by their name or key, instead
+# of by index.
+# Imagine table of contents in a
+# book, you have 'Chapter 6' on
+# page 263, so the key is 'Chapter
+# 6', the value is 263.
+# Or in the back of the book, you
+# have an Index, where you can see
+# which word is on which pages
+# like "strings" -> pages 2,3,6
+
+
 x = {
   'a': 'hello',
   'b': 'world'
 }
 
-r = x['a'] +  x['b'] 
-print(len(r))
+print(x['a'])
