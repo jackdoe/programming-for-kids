5431188b42b643f6d7de06d499dee52403cda122
borislav nikolov <borislav@fastmail.com>
2021-11-23 20:21:39 +0100

    typo

--- projects/programming-time/decks/easy/22.py
+++ projects/programming-time/decks/easy/22.py
@@ -2,7 +2,7 @@
 # by character, and multiply
 # it by 10 with len(s) - i
 # 10**3 means 10 to the power
-# of 3, or 10*10*10, or 300
+# of 3, or 10*10*10, or 100
 #
 # To convert the char '3' ASCII 51
 # to number 3 you have to subtract
