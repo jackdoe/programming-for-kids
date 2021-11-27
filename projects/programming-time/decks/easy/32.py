e985787e482761d2eb9a486cf790ba993fe36862
borislav nikolov <borislav@fastmail.com>
2021-11-27 13:29:27 +0100

    wip

--- projects/programming-time/decks/easy/32.py
+++ projects/programming-time/decks/easy/32.py
@@ -1,6 +1,23 @@
+# to see if something is contained
+# in a list, we go over each
+# element in the list, and see if
+# its equal to the element we are
+# looking for.
+# pretty similar to how you would
+# find needle in a haystack.
+def contains(haystack, needle):
+  for x in haystack:
+    if x == needle:
+      return True
+
+  return false
+
 a = [7,2,5,8]
-b = 2
+b = 5
 
-b += 1
+# or just use 'if b in a' 
+if contains(a, b):
+  print(b)
+else:
+  print(b * b)
 
-print(a[b])
