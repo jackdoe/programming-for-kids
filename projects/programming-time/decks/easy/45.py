a827cb246efe8b524c3f66b3df0fdff1a9ee5821
borislav nikolov <borislav@fastmail.com>
2021-11-27 11:51:03 +0100

    observable

--- projects/programming-time/decks/easy/45.py
+++ projects/programming-time/decks/easy/45.py
@@ -1,6 +1,28 @@
-i = 0
-while True:
-  i += 1
-  if i > 3:
-    break
-print(i)
+# Functions can also be values.
+# We can add function to a list.
+# For example, if we want to
+# send a message, and let functions
+# react to the message, its a common
+# pattern, to just have a list of
+# functions and call each one with
+# the message.
+
+observers = []
+
+def observerA(x):
+  if x == 5:
+    print(x)
+
+def observerB(x):
+  if x == 3:
+    print(x)
+
+def send(v):
+  for o in observers:
+    o(v)
+
+observers.append(observerA)
+observers.append(observerB)
+
+send(6)
+send(5)
