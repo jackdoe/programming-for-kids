e87d0d7aaf500c47a376740d23a6be32b937d0cf
borislav nikolov <borislav@fastmail.com>
2021-11-27 13:59:12 +0100

    concat lists

--- projects/programming-time/decks/easy/35.py
+++ projects/programming-time/decks/easy/35.py
@@ -1,3 +1,13 @@
-x = [1,2,3,4]
+# Destructuring (card 041) is very
+# handy to also expand lists in
+# for loops as well.
+# In this case the inner list will
+# be unpacked in the variables x
+# and y
 
-print(x[len(x)-2])
+points = [[0,1],[1,2],[3,2]]
+
+for x,y in points:
+  if x == 3 and y == 2:
+    print(x)
+
--- projects/programming-time/decks/easy/36.py
+++ projects/programming-time/decks/easy/36.py
@@ -1,3 +1,28 @@
-x = [1,2,3,4]
+# Concatinate two lists into a new
+# list is very similar to
+# concatinating two strings.
+#  * make a new list
+#  * append elements from listA
+#  * append elements from listB
+# however you might also decide
+# just to append all elements from
+# listB into the back of listA
+# this however will change listA
+# and sometimes you dont want that.
+def concat(listA, listB):
+  r = []
 
-print(x[3] - x[1])
+  for element in listA:
+    r.append(element)
+
+  for element in listB:
+    r.append(element)
+
+  return r
+
+a = [1,2]
+b = [3,4]
+
+c = concat(a,b)
+
+print(len(c))
