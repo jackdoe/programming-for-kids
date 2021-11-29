634b47b014feb67a6b1007789bd99b5fb94fd807
borislav nikolov <borislav@fastmail.com>
2021-11-29 15:27:19 +0100

    better explanation

--- projects/programming-time/decks/easy/00.py
+++ projects/programming-time/decks/easy/00.py
@@ -1,17 +1,15 @@
 # PROGRAMMING TIME (1)
 # [DECK: BASICS OF BASICS]
-# Start with 1 card per player,
-# put the rest face down (numbers
-# up).
+#
+# Start with 1 card per player, put
+# the rest face down (code down).
 #
 # Each player has to interpret the
-# code on their card, and look
-# for the output at the top most
-# card of the deck.
-#
-# First person to see their output
-# takes the card from the deck and
-# it becomes their active  card.
+# code on their card, and say the
+# output, if the output is correct,
+# pick the top most card from the
+# deck. You can check if the output
+# is correct on the joker cards.
 #
 # Player with most cards wins.
 
