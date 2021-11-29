d8a8cd9562938e8f1fcc5d2d240308ff44c429e0
borislav nikolov <borislav@fastmail.com>
2021-11-29 11:08:37 +0100

    explain more

--- projects/programming-time/decks/easy/15.py
+++ projects/programming-time/decks/easy/15.py
@@ -12,6 +12,11 @@
 # Even though the raw values are
 # the same, the types are not, so
 # python knows that '7' is not 55.
+# To get the ASCII code of a
+# character use ord(x), or if you
+# want to go the other way around,
+# use chr(y) to go from ASCII code
+# to character.
 
 x = '7'
 y = 55
