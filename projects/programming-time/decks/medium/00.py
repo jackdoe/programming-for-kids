1604393aa814804742d5c67259d0ebf4e89d7687
borislav nikolov <borislav@fastmail.com>
2021-11-29 15:36:05 +0100

    better explain

--- projects/programming-time/decks/medium/00.py
+++ projects/programming-time/decks/medium/00.py
@@ -1,26 +1,28 @@
-# PROGRAMMING TIME
-# 
-# 
-# Start with 1 card per
-# player, put the rest
-# face down (numbers 
-# up).
+# PROGRAMMING TIME (1)
+#   DECK: MEDIUM
+#         MADA MADA DANE
 #
-# Each player has to
-# interpret the code on
-# their card, and look
-# for the output at
-# the top most card of
-# the deck.
+# Start with 1 card per player, put
+# the rest face down (code down).
 #
-# First person to see
-# their output, takes
-# the card from the deck
-# and it becomes their
-# active card.
-# 
-# Player with most cards
-# wins.
+# Each player has to interpret the
+# code on their card, and say the
+# output, if the output is correct,
+# pick the top most card from the
+# deck. You can check if the output
+# is correct on the joker cards.
+#
+# Player with most cards wins.
 
+# Cheat Sheet:
+#   Value Type      |    ASCII
+#   1 -> integer    |    a -> 97
+#   2 -> string     |    z -> 122
+#   3 -> range      |    0 -> 48
+#   4 -> list       |    9 -> 57
+#   5 -> boolean    |
+# license: CC BY 4.0
+# programmingtime@fastmail.com
+# punkjazz.org/programming-time
 
 print(3)
