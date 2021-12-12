781e420bc7c9db3989fa5b20f4cd236596525e85
borislav nikolov <borislav@fastmail.com>
2021-12-12 11:21:27 +0100

    wip

--- projects/programming-time/decks/easy/14.py
+++ projects/programming-time/decks/easy/14.py
@@ -8,22 +8,21 @@ a = [pick(), pick(), pick()]
 # the list object has a method
 # `sort`, that sorts inplace, so it
 # will just modify the list itself,
-# without returning anything.
-# After that the list will be sorted
-# in ascending order (smaller
-# numbers first).
+# without returning anything.This is
+# called mutating, like the teenage
+# mutant ninja turtles, they changed
+# from turtles to ninja turtles, so
+# they mutated.  After that the list
+# will be sorted in ascending order
+# (smaller numbers first).
 a.sort()
 
 # reverse does what it says, inplace
 # it will reverse the list, it takes
 # first and last elements and swaps
 # them, then the second and
-# pre-last, and so on. Inplace, so
-# the list itself is modified. This
-# is called mutating, like the
-# teenage mutant ninja turtles, they
-# changed from turtles to ninja
-# turtles, so they mutated.
+# pre-last, and so on. Again
+# mutating the list.
 a.reverse()
 
 print(a[0])
