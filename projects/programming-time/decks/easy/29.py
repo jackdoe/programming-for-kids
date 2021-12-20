feb7a3b25a0f463d9c6b046a1b79dd2512f03f50
borislav nikolov <borislav@fastmail.com>
2021-12-20 16:04:52 +0100

    more interesting

--- projects/programming-time/decks/easy/29.py
+++ projects/programming-time/decks/easy/29.py
@@ -1,6 +1,11 @@
 # depending on the action apply
 # different operation to the value
 def reduce(v, action):
+  # check if the action is a
+  # function
+  if callable(action):
+    return action(v)
+    
   if (action["type"] == "INC"):
     return v + 1
 
@@ -13,5 +18,12 @@ dice = reduce(dice, {"type":"INC"})
 dice = reduce(dice, {"type":"DEC"})
 dice = reduce(dice, {"type":"INC"})
 dice = reduce(dice, {"type":"INC"})
+
+# You can make anonymous or nameless
+# function with the `lambda`
+# keyword, in this case we a
+# function that takes one parameter
+# x and returns x/2.
+dice = reduce(dice, lambda x: x/2)
 
 print(dice)
