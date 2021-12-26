b13dddb9bc29776d2138a2796868867edd62cea7
borislav nikolov <borislav@fastmail.com>
2021-12-26 17:49:32 +0100

    explain more

--- projects/programming-time/decks/easy/10.py
+++ projects/programming-time/decks/easy/10.py
@@ -1,9 +1,18 @@
-# reverse polish notation calculator
+# Reverse Polish Notation.
+# See `(3 + 2) * 4` in different
+# notations:
+#   Prefix    |  Infix  | Postfix
+#    (PN)     |         |  (RPN)
+# ------------+---------+----------
+# * (+ 2 4) 4 | (3+2)*4 | 3 2 + 4 *
+# ---------------------------------
+
 calc = [⚂, ⚂, '+', ⚂, '*', 3, '-']
 
 # * take 2 numbers from the front
 # * take 1 operation
-# * compute the result and put back
+# * compute the result and put it
+#   in the beginning of the list
 # * print the result if only
 #   one element is left
 while True:
@@ -16,8 +25,6 @@ while True:
     calc.insert(0, a - b)
   if op == '*':
     calc.insert(0, a * b)
-  if op == '/':
-    calc.insert(0, a / b)
     
   if len(calc) == 1:
     print(calc[0])
