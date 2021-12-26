370458e857bacce9f7afc4ace41983b2f320c524
borislav nikolov <borislav@fastmail.com>
2021-12-26 16:54:59 +0100

    wip

--- projects/programming-time/decks/easy/02.py
+++ projects/programming-time/decks/easy/02.py
@@ -1,21 +1,30 @@
-# if the door is locked:
-#   knock
-# else:
-#   enter
-x = ⚂
-y = ⚂
-if 1 == 2 or 2 == 3:
-  print('hey')
-else:
-  if x == y:
-    print('hi')
-  else:
-    if x > 10 or y < 10:
-      print('hoi')
-    else:
-      if x > 5 and y > 5:
-        print('hola')
-      elif x < 10 and y > 10:
-        print('heya')
-      else:
-        print('heyhey')
+# a dictionary is a way to lookup
+# keys and values in this example we
+# have the Care Bear name as key
+# and its color as value
+bears = {
+  "Bedtime Bear": "Aqua blue",
+  "Birthday Bear": "Golden yellow",
+  "Cheer Bear": "Carnation pink",
+  "Friend Bear": "Peach",
+  "Funshine Bear": "Lemon yellow",
+  "Good Luck Bear": "Green",
+  "Grumpy Bear": "Indigo blue",
+  "Love-A-Lot Bear": "Magenta pink",
+  "Tenderheart Bear": "Brown",
+}
+
+favorite = [
+  "Cheer Bear",
+  "Love-A-Lot Bear",
+  "Friend Bear"
+]
+
+# Pick a random bear from the list
+# of favorite names
+you = favorite[⚂ % len(favorite)]
+
+# for example bears["Grumpy Bear"]
+# is "Indigo Blue"
+color = bears[you]
+print('your color: ' + color)
