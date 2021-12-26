6e385ce4e91fb0f21f1e2ab038cad3bb16780d0a
borislav nikolov <borislav@fastmail.com>
2021-12-26 13:55:37 +0100

    wip

--- projects/programming-time/decks/easy/51.py
+++ projects/programming-time/decks/easy/51.py
@@ -1,17 +1,31 @@
-# choose a random element from a
-# list
-def choice(data):
-  # in the real world
-  # imagine a 4294967296 sided dice
-  # but in this game, 20 sided will
-  # have to do
-  dice = ⚂
-  return data[dice % len(data)]
-
-
-name = choice(["Alice", "Bob"])
-age = choice([9, 10, 11, 12])
-
-print("Hello!")
-print("Name: " + name)
-print("Age: " + str(age))
+# A LinkedList is a data structure,
+# where each element has a value and
+# a pointer to the next element. The
+# lists you usually use are called
+# arrays where you can get to each
+# element by its index, in a linked
+# list you can not directly go to
+# specific index, unless you follow
+# the chain.
+class LinkedList:
+  def __init__(self, next, value):
+    self.next = next
+    self.value = value
+# HINT: use pen and paper to follow
+root = LinkedList(
+  LinkedList(
+    LinkedList(
+      None,
+      ⚂
+    ),
+    ⚂,
+  ),
+  ⚂
+)
+# start from the root and follow the
+# chain of .next elements until we
+# reach None.
+element = root
+while element != None:
+  print(element.value)
+  element = element.next
