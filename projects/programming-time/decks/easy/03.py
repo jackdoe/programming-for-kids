e71c51bd333a2dae91c88661b40ddd6bbb54fb67
borislav nikolov <borislav@fastmail.com>
2021-12-26 22:15:58 +0100

    make the first card more explanatory

--- projects/programming-time/decks/easy/03.py
+++ projects/programming-time/decks/easy/03.py
@@ -1,24 +1,30 @@
-# a list where we will keep all the
-# rolls
-rolls = []
+# Prime number is a number that is
+# divisible, without remainder, only
+# by itself and 1 (e.g. 2, 3, 5, 7,
+# 11). To check the remainder use
+# the modulo operator %. For example
+# 5 is a prime number:
+#  5 % 1 is 0 (reminder of 5/1 is 0)
+#  5 % 2 is 1 (reminder of 5/2 is 1)
+#  5 % 3 is 2 (reminder of 5/3 is 2)
+#  5 % 4 is 1 (reminder of 5/4 is 1)
+#  5 % 5 is 0 (reminder of 5/5 is 0)
+def is_prime(n):
+  # 0 and 1 are not prime
+  if n == 0 or n == 1:
+    return False
+  # check if the number is divisable
+  # by any number from 2 to n
+  for x in range(2,n):
+    # if there is no reminder that
+    # means it is divisable, and it
+    # is not a prime
+    if n % x == 0:
+      return False
+    return True
 
-# 4 % 3 means divide 4 by 3 and take
-# the remainder.
-n = (⚂ % 4)
-if n < 2:
-  n = 2
-
-# start from i = 0 up to, but
-# excluding, n, increase `i` with 2
-# at each step
-for i in range(0, n, 2):
-  # roll the dice every time
-  rolls.append(⚂)
-
-# sort the list in ascending order
-# that means [3,1,2,5] will become
-# [1,2,3,5]
-rolls.sort()
-
-# print the element at index 0
-print(rolls[0])
+n = ⚂
+if is_prime(n):
+  print('prime')
+else:
+  print('not a prime')
