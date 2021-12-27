26fc9e25492c52aa172d9640a834da45140c38dc
borislav nikolov <borislav@fastmail.com>
2021-12-27 08:34:06 +0100

    fix

--- projects/programming-time/decks/easy/04.py
+++ projects/programming-time/decks/easy/04.py
@@ -1,21 +1,21 @@
 # The RSA algorithm is possibly the
 # most used encryption algorithm in
-# the world.
-# in our example we use small prime
-# numbers, but 1528622835711533 and 
+# the world.  In our example we use
+# small prime numbers, but 3389 and
 # 2178583086560517094530555255075673
-# are also prime numbers.
-
+# are also prime numbers. Usually
+# RSA uses prime numbers with more
+# than 300 digits (1024 bit numbers)
 p = 11 # must be prime number
 q = 17 # must be prime number
 e = 7 # prime number between
-      # (p-q)*(q-1), in our case
+      # (p-1)*(q-1), in our case
       # (11-1)*(17-1) = 160
       # so `e` must be a prime
       # number > 1 and < 160,
       # so I picked 7 as its also
       # my lucky number.
-N = p * q # 187
+N = p * q # 187, called modulus
 
 message = ⚂
 
