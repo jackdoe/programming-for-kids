1b083eaa136356cd066e3963f47060b4bd638ee1
borislav nikolov <borislav@fastmail.com>
2021-12-30 16:04:05 +0100

    λ

--- projects/programming-time/decks/easy/17.py
+++ projects/programming-time/decks/easy/17.py
@@ -1,31 +1,31 @@
-# lists are just a ordered sequence
-# of things, they can also contain
-# other lists
-image = [
-  [1,0,0,0,1],
-  [0,1,0,1,0],
-  [0,0,1,0,0],
-  [0,1,0,1,0],
-  [1,0,0,0,1],
-]
+# λ (lambda) calculus
+# "Most computer languages (like C
+# and C++ or Java) are invented, λ
+# calculus is discovered."
+#     -- Phillip Wadler
+# This example is a snippet from
+# gist.github.com/gcr/848437
+TRUE  = lambda t: lambda f: t()
+FALSE = lambda t: lambda f: f()
+IF =                              \
+   lambda cond:                   \
+     lambda t:                    \
+     lambda f:                    \
+       cond(lambda: t)(lambda: f)
+AND =                             \
+  lambda a:                       \
+    lambda b:                     \
+      IF(a)(b)(FALSE)
+OR =                              \
+  lambda a:                       \
+    lambda b:                     \
+      IF(a)(TRUE)(b)
 
-# height is the number of rows
-height = len(image)
-# we assume image is rectangle
-width = len(image[0])
-
-# image[0] returns a list, so
-# image[0][1] will return the second
-# element from the first row
-image[⚂ % height][⚂ % width] = 2
-
-for row in image:
-  for pixel in row:
-    if pixel == 0:
-      print(' ', end='')
-    elif pixel == 1:
-      print('@', end='')
-    else:
-      print('%', end='')
-  # print new line after every row
-  print('')
+print(IF                          \
+      (AND                        \
+         (OR                      \
+            (TRUE)                \
+            (FALSE))              \
+         (TRUE))                  \
+      (lambda: ⚂)                 \
+      (lambda: ⚂)())
