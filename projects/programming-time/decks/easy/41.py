0cf57671401479119ee9941a6ae34f0a1376cadc
borislav nikolov <borislav@fastmail.com>
2022-01-07 23:41:12 +0100

    move destructuring into cheatsheet; add pi and e

--- projects/programming-time/decks/easy/41.py
+++ projects/programming-time/decks/easy/41.py
@@ -1,20 +1,31 @@
-# This is called 'Destructuring'
-# when on the left side you have a
-# bunch of things and on the right
-# you have something that can be
-# unpacked.
-#
-#  data = [1,2]
-#  a,b = data
-# is the same as
-#  data = [1,2]
-#  a = data[0]
-#  b = data[1]
-#
-# it is quite handy to unpack lists
-# like that
+# Compute Euler's number
+# e = 1 + 1/1 + 1/2 + 1/ (1*2*3) …
+def compute_e(end):
+  e = f = 1.0
+  for i in range(2, end):
+    e += 1.0 / f
+    f *= i # compute the factorial
+           # incrementally
+  return e
 
-a,b,c = [⚂,⚂,⚂]
-print(a + b + c)
+# Compute 𝜋
+# Leibniz's formula to compute 𝜋
+# 𝜋 = 4 – 4/3 + 4/5 – 4/7 + 4/9 – …
+def compute_pi(end):
+  pi = 0
+  n = 4
+  d = 1
+  for i in range(1,end):
+    # alternate between -1 and 1
+    # (2 * (0 % 2)) - 1 = -1
+    # (2 * (1 % 2)) - 1 = 1
+    # (2 * (2 % 2)) - 1 = -1
+    # (2 * (3 % 2)) - 1 = 1
+    sign = (2*(i % 2)) - 1
+    pi += sign * n/d
+    d+=2
+  return pi
 
-
+e = compute_e(3 + ⚂ % 3)
+pi = compute_pi(2 + ⚂ % 3)
+print(e,pi)
