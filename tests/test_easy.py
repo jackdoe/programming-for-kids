3ee732d763582a0a9a40186d2c633a9ad48be79b
Jim Cramer <remarcmij@gmail.com>
2022-01-08 16:20:23 +0100

    Added first tests

--- tests/test_easy.py
+++ tests/test_easy.py
@@ -1,5 +1,12 @@
-"""Test suite for easy deck"""
+"""
+Test suite for easy deck
+
+If needed, install pytest: pip install pytest
+
+Then run the command: pytest
+"""
 import os
+import re
 import sys
 from io import StringIO
 
@@ -13,16 +20,27 @@ def run_card(file_name, values=range(1, 21)):
             sys.path[0], "../projects/programming-time/decks/easy", file_name + ".py"
         )
     )
-    prog_text = open(file_path, "r", encoding="utf-8").read()
 
+    with open(file_path, "r", encoding="utf-8") as file:
+        lines = []
+        for line in file:
+            line = line.rstrip()
+            # remove blank and comment lines
+            if line != "" and not re.match(r"^\s*#", line):
+                lines.append(line)
+        prog_text = "\n".join(lines)
+
+    # replace PLACE_HOLDER symbols with actual values
     value_index = 0
     while prog_text.find(PLACE_HOLDER) != -1:
         prog_text = prog_text.replace(PLACE_HOLDER, str(values[value_index]), 1)
         value_index += 1
 
+    # capture stdout output
     old_stdout = sys.stdout
     sys.stdout = my_stdout = StringIO()
 
+    # pylint: disable = exec-used
     exec(prog_text)
 
     sys.stdout = old_stdout
