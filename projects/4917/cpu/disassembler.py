c5a8f47f43b53e06c9383d323bb0c96bc45de875
Jim Cramer <remarcmij@gmail.com>
2022-03-23 09:15:18 +0100

    wip

--- projects/4917/cpu/disassembler.py
+++ projects/4917/cpu/disassembler.py
@@ -25,72 +25,105 @@ def is_ip_aligned(IP, memory):
     return ip_aligned
 
 
+def get_new_start(IP, memory):
+    index = IP
+    prev_index = index
+    while index > 0:
+        prev_index = index
+        IS = memory[index]
+        mnemonic, type = mnemonics[IS]
+        if type == InstructionType.STATELESS:
+            index -= 1
+        elif type == InstructionType.REGISTER:
+            index -= 1
+        elif type == InstructionType.IO:
+            index -= 2
+        elif type == InstructionType.MEMORY:
+            index -= 2
+        elif type == InstructionType.BRANCH:
+            index -= 2
+    return prev_index
+
+
 def disassemble(state, highlight, cycle=0):
     IP, IS, R0, R1, memory = state
 
+    count = 0
+    address = 0 if is_ip_aligned(IP, memory) else IP
+
+    # if count != address:
+    #     print("new_start", get_new_start(IP, memory))
+
     print("-" * 23)
 
-    index = 0 if is_ip_aligned(IP, memory) else IP
-
-    while index < len(memory):
-
-        IS = memory[index]
+    while count < len(memory):
+        IS = memory[address]
         mnemonic, type = mnemonics[IS]
-        marker = ">" if index == IP else " "
+        marker = ">" if address == IP else " "
 
         if type == InstructionType.STATELESS:
             # no operand
-            print(f"{marker}{index:02d}: {IS:02d}     {mnemonic:5}")
-            index += 1
+            print(f"{marker}{address:02d}: {IS:02d}     {mnemonic:5}")
+            address += 1
+            count += 1
         elif type == InstructionType.REGISTER:
             match = REGISTER_PATTERN.match(mnemonic)
             if not match:
                 raise Exception("Internal error in disassembler")
             mnemonic = match.group(1)
             register = match.group(2)
-            print(f"{marker}{index:02d}: {IS:02d}     {mnemonic:5} {register}")
-            index += 1
+            print(f"{marker}{address:02d}: {IS:02d}     {mnemonic:5} {register}")
+            address += 1
+            count += 1
         elif type == InstructionType.IO:
-            if index >= len(memory) - 1:
+            if count >= len(memory) - 1:
                 # End of memory, no operand: treat as data
-                print(f" {index:02d}: {IS:02d}     ???")
-                index += 1
+                print(f" {address:02d}: {IS:02d}     ???")
+                address += 1
+                count += 1
             else:
-                operand = memory[index + 1]
+                operand = memory[address + 1]
                 print(
-                    f"{marker}{index:02d}: {IS:02d} {operand:02}  {mnemonic:5} {operand}"
+                    f"{marker}{address:02d}: {IS:02d} {operand:02}  {mnemonic:5} {operand}"
                 )
-                index += 2
+                address += 2
+                count += 2
         elif type == InstructionType.MEMORY:
-            if index >= len(memory) - 1:
+            if count >= len(memory) - 1:
                 # End of memory, no operand: treat as data
-                print(f" {index:02d}: {IS:02d}     ???")
-                index += 1
+                print(f" {address:02d}: {IS:02d}     ???")
+                address += 1
+                count += 2
             else:
-                operand = memory[index + 1]
+                operand = memory[address + 1]
                 match = REGISTER_PATTERN.match(mnemonic)
                 if not match:
                     raise Exception("Internal error in disassembler")
                 mnemonic = match.group(1)
                 register = match.group(2)
                 print(
-                    f"{marker}{index:02d}: {IS:02d} {operand:02}  {mnemonic:5} {register},{operand}"
+                    f"{marker}{address:02d}: {IS:02d} {operand:02}  {mnemonic:5} {register},{operand}"
                 )
-                index += 2
+                address += 2
+                count += 2
         elif type == InstructionType.BRANCH:
             # one operand
-            if index >= len(memory) - 1:
+            if count >= len(memory) - 1:
                 # End of memory, no operand: treat as data
-                print(f" {index:02d}: {IS:02d}")
-                index += 1
+                print(f" {address:02d}: {IS:02d}     ???")
+                address += 1
+                count += 1
             else:
-                operand = memory[index + 1]
+                operand = memory[address + 1]
                 print(
-                    f"{marker}{index:02d}: {IS:02d} {operand:02}  {mnemonic:5} {operand}"
+                    f"{marker}{address:02d}: {IS:02d} {operand:02}  {mnemonic:5} {operand}"
                 )
-                index += 2
+                address += 2
+                count += 2
 
         else:
             raise Exception(f"Invalid instruction type: {type}")
+
+        address &= 0xF
 
     print(f"\n R0: {R0}, R1: {R1}, Cycle: {cycle}\n")
