cca9237f50703e03d43e02b642d65536de3c1d3d
borislav nikolov <borislav@fastmail.com>
2022-07-31 17:27:00 +0200

    move

--- make.sh
+++ make.sh
@@ -1,11 +1,8 @@
 echo > book.md
-echo > README.md
 python3 toc.py
 for i in  cover.md hello.md toc.md $(ls -1 week-*.md | sort -k 2 -t- -n); do
     cat $i >> book.md
     echo >> book.md
 done
-
-cat cover.md hello.md frontpage.md > README.md
 
 python3 extract.py
