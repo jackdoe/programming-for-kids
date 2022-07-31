841ac56c4e53cf52fe5b8f11aab906c9d9f8e9bb
borislav nikolov <borislav@fastmail.com>
2022-07-31 16:42:44 +0200

    split the readme, its too big

---
+++ frontpage.md
@@ -0,0 +1,7 @@
+--
+
+The whole book: [https://github.com/jackdoe/programming-for-kids/blob/master/book.md](book.md)
+
+if you have any ideas or feedback, make a pull request, or send me an email to programmingtime@fastmail.com
+
+
--- make.sh
+++ make.sh
@@ -1,8 +1,11 @@
+echo > book.md
 echo > README.md
 python3 toc.py
 for i in  cover.md hello.md toc.md $(ls -1 week-*.md | sort -k 2 -t- -n); do
-    cat $i >> README.md
-    echo >> README.md
+    cat $i >> book.md
+    echo >> book.md
 done
+
+cat cover.md hello.md toc.md frontpage.md > README.md
 
 python3 extract.py
