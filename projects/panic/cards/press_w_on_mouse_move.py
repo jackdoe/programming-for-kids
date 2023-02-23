ffef234b8b7fd78f8ee4a62cff485c9c4f99575e
borislav nikolov <borislav@fastmail.com>
2023-02-23 18:48:59 +0100

    wip

---
+++ projects/panic/cards/press_w_on_mouse_move.py
@@ -0,0 +1,30 @@
+import pynput.mouse as m
+import pynput.keyboard as k
+
+# press w while the mouse is moving
+# its good to combine this with
+# is_foreground() to run only while some
+# game is active
+
+mouse = m.Controller()
+kbd = k.Controller()
+
+key = k.KeyCode.from_char('w')
+def on_move(x, y):
+  # while the mouse is moving
+  # we keep pressing w
+  kbd.press(key)
+  kbd.release(key)
+
+def on_click(x, y, button, pressed):
+  pass
+
+def on_scroll(x, y, dx, dy):
+  pass
+
+with m.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as l:
+    l.join()
+
