81f8832a397365f9db3e09df2fc77bc5d58695c6
borislav nikolov <borislav@fastmail.com>
2023-02-23 17:48:54 +0100

    wip

--- projects/panic/cards/no_going_back.py
+++ projects/panic/cards/no_going_back.py
@@ -1,27 +1,29 @@
-# pip install pynput
+# pip install pynput win32gui
 from pynput import keyboard
+import win32gui
+# while minecraft is focused, disable
+# the S key, so you cant go back
 
-# this card ignores the 'S' key
-# so in video games you cant go
-# backwards
+def is_foreground(name):
+  w = win32gui.GetForegroundWindow()
+  title = win32gui.GetWindowText(w)
+  if name in title:
+    return True
+  return False
 
 listener = None
-
 def filter(msg,data):
-  # 0x53 is S's virtual code
-  # on windows
-  if data.vkCode == 0x53:
-    listener.suppress_event()
-
+  # 0x53 is S's virtual code on windows
+  if is_foreground("Minecraft"):
+    if data.vkCode == 0x53:
+      listener.suppress_event()
 def on_press(key):
   pass
-
 def on_release(key):
   pass
 
 listener = keyboard.Listener(
   win32_event_filter=filter,
-  supress=False,
   on_press=on_press,
   on_release=on_release)
 listener.start()
