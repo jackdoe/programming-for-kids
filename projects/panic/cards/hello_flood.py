98338be6d716b2e57a1335ab59dc4560d9619dc4
borislav nikolov <borislav@fastmail.com>
2023-02-26 22:16:46 +0100

    wip

---
+++ projects/panic/cards/hello_flood.py
@@ -0,0 +1,18 @@
+import random
+import win32gui as g
+
+# flood the screen with the text
+# 'Hello?'
+
+dc = g.GetDC(0)
+text = "Hello?"
+
+while True:
+  x = random.randint(0, 6000)
+  y = random.randint(0, 6000)
+  g.DrawText(dc, 
+             text, 
+             len(text), 
+             (x,y,x+100,y+100),
+             0)
+  
