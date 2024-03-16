f3473b85a105de5d195191d058d272581b56058f
borislav nikolov <borislav@fastmail.com>
2024-03-16 12:55:33 +0100

    cleanup

--- examples/week-006/011.txt
+++
@@ -1,2 +0,0 @@
-import turtle
-turtle.circle(30)
--- make.sh
+++ make.sh
@@ -1,8 +1,6 @@
-echo > book.md
-python3 toc.py
-for i in  cover.md hello.md toc.md $(ls -1 week-*.md | sort -k 2 -t- -n); do
+cat cover.md hello.md > book.md
+for i in $(ls -1 weeks/week-*.md | sort -k 2 -t- -n); do
     cat $i >> book.md
     echo >> book.md
 done
 
-python3 extract.py
